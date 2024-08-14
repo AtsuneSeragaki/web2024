f67578f42e3dce21aaebfce58bd9cbca7f19ddd4
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2024-08-14 12:06:04 +0900

    8/14 課題

---
+++ textbook/moguratataki/dialog.js
@@ -0,0 +1,13 @@
+var tentokun;
+
+window.onload = function(){
+    start();
+}
+
+function start(){
+    tentokun = document.getElementById("gazo");
+    tentokun.onclick = function(){
+        alert("たたかれた！");
+    };
+}
+
